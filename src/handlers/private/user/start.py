476939fe02fd1c75f14f36af395614a11d41bc5e
umarr <umarr.developer@gmail.com>
2024-06-17 15:21:25 +0600

    edit start handler

--- src/handlers/private/user/start.py
+++ src/handlers/private/user/start.py
@@ -1,10 +1,20 @@
 from aiogram import Router, types
 from aiogram.filters import Command
 
+from src.handlers.private.user.menu import menu
+from src.models import User
+
 router = Router()
 
 
 @router.message(Command(commands=['start']))
-async def on_start(message: types.Message):
-    text = 'Привет пользователь'
-    await message.answer(text)
+async def on_start(message: types.Message, user: User, db):
+    if not user:
+        user_id = message.from_user.id
+        await User.new(db, user_id=user_id)
+
+        text = 'Добро пожаловать, новый пользователь'
+        await message.answer(text)
+
+    else:
+        await menu(message)
