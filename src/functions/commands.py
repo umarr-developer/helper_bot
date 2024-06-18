862d78bcb4e81c12850e0545c4c56f6612db9a73
umarr <umarr.developer@gmail.com>
2024-06-18 12:01:07 +0600

    add new command for FAQ handler

--- src/functions/commands.py
+++ src/functions/commands.py
@@ -5,7 +5,8 @@ async def set_commands(bot: Bot):
     await bot.set_my_commands(commands=[
         types.BotCommand(command='start', description='Запустить бота'),
         types.BotCommand(command='menu', description='Перейти в меню'),
-        types.BotCommand(command='about', description='Показать информацию о боте')
+        types.BotCommand(command='about', description='Показать информацию о боте'),
+        types.BotCommand(command='faq', description='Ответы на часто задаваемые вопросы')
     ],
         scope=types.BotCommandScopeAllPrivateChats()
     )
