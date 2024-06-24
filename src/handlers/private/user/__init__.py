eb0e9f93553d950b246cae33c6e2559363c37a56
umarr <umarr.developer@gmail.com>
2024-06-24 11:07:59 +0600

    set BlockUserMiddleware for user handlers

--- src/handlers/private/user/__init__.py
+++ src/handlers/private/user/__init__.py
@@ -1,10 +1,11 @@
 from aiogram import Router
 
 from src.handlers.private.user import start, menu, about, faq
-from src.middlewares import ThrottlingMiddleware
+from src.middlewares import ThrottlingMiddleware, BlockedUserMiddleware
 
 router = Router()
 router.message.middleware(ThrottlingMiddleware())
+router.message.middleware(BlockedUserMiddleware())
 router.include_router(start.router)
 router.include_router(menu.router)
 router.include_router(about.router)
