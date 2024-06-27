ccaabb728a9bb44c2c6fc3b95a2cd0ab10ea6a60
umarr <umarr.developer@gmail.com>
2024-06-27 14:14:09 +0600

    update on_admin_cancel handler

--- src/handlers/private/admin/cancel.py
+++ src/handlers/private/admin/cancel.py
@@ -1,12 +1,15 @@
 from aiogram import F, types, Router
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
 
 router = Router()
 
 
 @router.message(Command(commands=['cancel']))
 @router.callback_query(F.data == 'admin_cancel')
-async def on_admin_cancel(message: types.Message | types.CallbackQuery):
+async def on_admin_cancel(message: types.Message | types.CallbackQuery, state: FSMContext):
+    await state.clear()
+
     if isinstance(message, types.CallbackQuery):
         await message.answer()
         await message.message.edit_reply_markup()
