6d9c5dcec62691d977f58ce7adfcdcdfb511909d
umarr <umarr.developer@gmail.com>
2024-06-27 16:43:12 +0600

    add manage_users to admin handlers

--- src/handlers/private/admin/__init__.py
+++ src/handlers/private/admin/__init__.py
@@ -1,7 +1,7 @@
 from aiogram import Router
 
 from src.filters import AdminFilter
-from src.handlers.private.admin import start, admin_panel, faq, cancel
+from src.handlers.private.admin import start, admin_panel, faq, cancel, manage_users
 
 router = Router()
 router.message.filter(AdminFilter())
@@ -10,3 +10,4 @@ router.include_router(start.router)
 router.include_router(admin_panel.router)
 router.include_router(faq.router)
 router.include_router(cancel.router)
+router.include_router(manage_users.router)
