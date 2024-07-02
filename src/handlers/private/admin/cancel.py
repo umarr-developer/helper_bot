4302d810edca4da7950e0c2841212994337b6e89
umarr <umarr.developer@gmail.com>
2024-07-02 16:12:35 +0600

    fix 'on_cancel' handler

--- src/handlers/private/admin/cancel.py
+++ src/handlers/private/admin/cancel.py
@@ -6,8 +6,8 @@ router = Router()
 
 
 @router.message(Command(commands=['cancel']))
-@router.callback_query(F.data == 'admin_cancel')
-async def on_admin_cancel(message: types.Message | types.CallbackQuery, state: FSMContext):
+@router.callback_query(F.data == 'cancel')
+async def on_cancel(message: types.Message | types.CallbackQuery, state: FSMContext):
     await state.clear()
 
     if isinstance(message, types.CallbackQuery):
