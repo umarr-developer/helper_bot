7759c12a058848312b40586939ba6a9e43a0f32d
umarr <umarr.developer@gmail.com>
2024-07-06 12:53:14 +0600

    update 'manage_operators' handlers

--- src/handlers/private/admin/manage_operators.py
+++ src/handlers/private/admin/manage_operators.py
@@ -15,18 +15,18 @@ class AddOperatorState(StatesGroup):
     username = State()
 
 
-@router.callback_query(F.data == 'admin_manage_operators', StateFilter(None))
+@router.callback_query(F.data == 'admin-manage-operators', StateFilter(None))
 async def on_manage_operators(callback: types.CallbackQuery):
     text = 'Выберите действия'
     keyboard = types.InlineKeyboardMarkup(
         inline_keyboard=[
             [
                 types.InlineKeyboardButton(text='Добавить оператора',
-                                           callback_data='admin_add_operator')
+                                           callback_data='add-operator')
             ],
             [
                 types.InlineKeyboardButton(text='Список операторов',
-                                           callback_data='admin_operator_list')
+                                           callback_data='operator-list')
             ]
         ]
     )
@@ -34,70 +34,77 @@ async def on_manage_operators(callback: types.CallbackQuery):
     await callback.message.answer(text, reply_markup=keyboard)
 
 
-@router.callback_query(F.data == 'admin_add_operator')
+@router.callback_query(F.data == 'add-operator')
 async def on_manage_operators_add_operator(callback: types.CallbackQuery, state: FSMContext):
     await callback.answer()
     await callback.message.edit_reply_markup()
 
-    text = 'Введите ID оператора. Оператор должен быть а базе данных пользователей!'
+    text = 'Введите <b>ID</b> оператора. 👨‍💼 <b>Оператор</b> должен быть а базе данных пользователей!'
     await callback.message.answer(text)
 
     await state.set_state('add_operator')
 
 
-@router.message(StateFilter('add_operator'))
-async def on_manage_operators_add_operator_search(message: types.Message, db: sessionmaker, state: FSMContext):
+@router.message(StateFilter('add-operator'))
+async def on_manage_operators_add_operator_search(message: types.Message,
+                                                  db: sessionmaker,
+                                                  state: FSMContext,
+                                                  bot: Bot):
     if not message.text.isdigit():
-        text = 'Некорректные данные. Попробуйте еще раз'
+        text = '🚫 Некорректные данные. Попробуйте еще раз'
         await message.answer(text)
         return
 
     user = await User.get(db, int(message.text))
 
     if not user:
-        text = 'Пользователь не найден. Попробуйте еще раз'
+        text = '🚫 Пользователь не найден. Попробуйте еще раз'
         await message.answer(text)
         return
 
     operator = await Operator.get(db, int(message.text))
     if not operator:
-        text = f'Найден пользователь c ID {user[0].user_id}'
+        chat = await bot.get_chat(message.from_user.id)
+        text = f'Найден пользователь c <b>ID {user[0].id}</b>\n\n' \
+               f'<b>USER ID</b>: <code>{user[0].user_id}</code>\n' \
+               f'<b>ФИО</b>: <code>{chat.full_name}</code>\n' \
+               f'<b>Дата регистрации в боте</b>: <code>{user[0].created_on}</code>\n\n'
         keyboard = types.InlineKeyboardMarkup(
             inline_keyboard=[
                 [
                     types.InlineKeyboardButton(text='Сделать оператором',
-                                               callback_data=f'admin-set-operator_{user[0].user_id}')
+                                               callback_data=f'set-operator_{user[0].user_id}')
                 ]
             ]
         )
 
         await message.answer(text, reply_markup=keyboard)
     else:
-        text = 'Этот пользователь уже оператор'
+        text = '❗️ Этот пользователь уже оператор'
         await message.answer(text)
 
     await state.clear()
 
 
-@router.callback_query(F.data.startswith('admin-set-operator'))
-async def on_manage_operators_set_operator(callback: types.CallbackQuery, db: sessionmaker, bot):
+@router.callback_query(F.data.startswith('set-operator'))
+async def on_manage_operators_set_operator(callback: types.CallbackQuery, db: sessionmaker):
     prefix, user_id = callback.data.split('_')
     await Operator.new(db, int(user_id))
 
-    text = 'Оператор добавлен'
+    text = '✅ Оператор добавлен'
     await callback.message.answer(text)
 
 
-@router.callback_query(F.data == 'admin_operator_list')
-@router.callback_query(F.data.startswith('admin-list-operators-action'))
+@router.callback_query(F.data == 'operator-list')
+@router.callback_query(F.data.startswith('list-operators-action'))
 async def on_manage_operator_list(callback: types.CallbackQuery, db: sessionmaker, bot: Bot):
-    buttons = await OperatorsListKeyboard.get_buttons(db, 'admin-list-operators', bot)
-    text = 'Лист операторов'
-    keyboard = OperatorsListKeyboard(buttons, 'admin-list-operators-action')
+    buttons = await OperatorsListKeyboard.get_buttons(db, 'list-operators', bot)
+    text = '📘 Лист операторов'
+    keyboard = OperatorsListKeyboard(buttons, 'list-operators-action')
 
-    if callback.data.startswith('admin-list-operators-action'):
+    if callback.data.startswith('list-operators-action'):
         prefix, index = callback.data.split('_')
         await callback.message.edit_text(text, reply_markup=keyboard.as_keyboard(int(index)))
         return
-    
+
     await callback.message.answer(text, reply_markup=keyboard.as_keyboard(0))
