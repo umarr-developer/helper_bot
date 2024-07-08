62a9a1d583fac62344cf37609a465735d8cfb9c9
umarr <umarr.developer@gmail.com>
2024-07-08 11:15:29 +0600

    add filter for Operators

--- src/filters/chat.py
+++ src/filters/chat.py
@@ -1,6 +1,10 @@
 from aiogram import Bot, types
 from aiogram.filters import BaseFilter
+from sqlalchemy.orm import sessionmaker
+
+from models import Operator
 from src.config import Config
+
 
 
 class AdminFilter(BaseFilter):
@@ -13,3 +17,13 @@ class UserFilter(AdminFilter):
 
     async def __call__(self, get: types.Message | types.CallbackQuery, config: Config):
         return not await super().__call__(get, config)
+
+
+class OperatorFilter(BaseFilter):
+
+    async def __call__(self, get: types.Message | types.CallbackQuery, db: sessionmaker):
+        user_id = get.from_user.id
+        operator = await Operator.get(db, user_id)
+        if operator:
+            return True
+        return False
